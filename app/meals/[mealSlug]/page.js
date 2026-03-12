322305cdb0c590118219749beae655a0732dd8b2
radeveljic99 <80167674+radeveljic99@users.noreply.github.com>
2026-03-12 18:17:06 +0100

    Initial commit

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailsPage() {
+  return (
+    <>
+      <h1>Meals Detail Page</h1>
+    </>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function ShareMealPage() {
+  return (
+    <>
+      <h1>Meals Share Page</h1>
+    </>
+  );
+}
