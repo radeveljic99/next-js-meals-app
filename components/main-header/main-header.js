e7d6b0decd9214db4820699490256b49664717ca
radeveljic99 <rade.veljic99@gmail.com>
2026-03-12 22:22:03 +0100

    feat: updates nav links

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,25 +1,31 @@
-import logoImg from '@/assets/logo.png';
-import Image from 'next/image';
+import logoImg from "@/assets/logo.png";
+import Image from "next/image";
 import Link from "next/link";
-import MainHeaderBackground from '../main-header-background/main-header-background';
-import classes from './main-header.module.css';
+import MainHeaderBackground from "../main-header-background/main-header-background";
+import classes from "./main-header.module.css";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
-  return <>
-    <MainHeaderBackground />
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg} priority alt="Logo" />
-        Next Level Food
-      </Link>
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} priority alt="Logo" />
+          Next Level Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li><Link href="/meals">Browse Meals</Link></li>
-          <li><Link href="/community">Foodies Community</Link></li>
-        </ul>
-      </nav>
-    </header>
-
-  </>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
