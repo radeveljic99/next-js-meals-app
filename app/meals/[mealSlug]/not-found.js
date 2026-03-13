c44bbb54b3331b5ed967d9a3bc278ca0459be3e7
radeveljic99 <80167674+radeveljic99@users.noreply.github.com>
2026-03-13 15:43:29 +0100

    feat: update project

---
+++ app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unforunatley, we could not find the resource</p>
+    </main>
+  );
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,36 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import Link from "next/link";
+import { Suspense } from "react";
+import classes from "./page.module.css";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
 export default function MealsPage() {
   return (
     <>
-      <h1>Meals Page</h1>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourseful. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
     </>
   );
 }
